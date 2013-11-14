d1d42b99e6fba3c08a9b002a4f9c5cb75e17dada
AndyHu <hushibei@tianji.com>
2013-11-14 15:29:46 +0800

    更新backup

--- lib/admin_backup.rb
+++ lib/admin_backup.rb
@@ -1,5 +1,5 @@
 class AdminBackup
-  REMOTE_PATH = "test/"
+  REMOTE_PATH = "andy_site_db/"
   def self.backup
     file_name = "/home/andy/backups/*/*/*.tar "
     stdout = Open4::popen4 "backup perform --trigger my_backup; ls #{file_name};baidupan upload #{file_name} #{REMOTE_PATH}"
