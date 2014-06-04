eba3e07b195af08cdc2d9b789e8f7669c9cca9bb
AndyHu <hushibei@tianji.com>
2014-06-04 15:32:50 +0800

    解决创建migrate 时 charset是拉丁的问题

--- db/migrate/018_create_time_axes.rb
+++ db/migrate/018_create_time_axes.rb
@@ -1,6 +1,6 @@
 class CreateTimeAxes < ActiveRecord::Migration
   def self.up
-    create_table  :time_axes do |t|
+    create_table  :time_axes, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8' do |t|
       t.string    :year
       t.string    :date
       t.string    :title
