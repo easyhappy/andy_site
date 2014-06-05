db6e7f69527aff8ef03dec0f6c60409b8be9de93
AndyHu <hushibei@tianji.com>
2014-06-05 15:49:05 +0800

    修改时光机 显示样式

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -63,7 +63,7 @@ AndySite.helpers do
       contents << content_tag(:li, link_to('博客', url('/blog')))
       contents << content_tag(:li, link_to('笔记', url('/note')))
       contents << content_tag(:li, link_to('分类', url('/tag')))
-      contents << content_tag(:li, link_to('关于', url('/me')))
+      contents << content_tag(:li, link_to('关于', url('/me'), :target => :blank))
     end
   end
 
